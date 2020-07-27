bf46e565b534490de76aa50f8d765d87803ac566
Andrew Rabert <ar@nullsum.net>
2020-07-27 14:22:24 -0400

    3.8.1

--- async_case.py
+++ async_case.py
@@ -1,11 +1,95 @@
 import asyncio
 import inspect
 
-from unittest.case import TestCase
+from unittest.case import TestCase, _Outcome
 
 
 
-class IsolatedAsyncioTestCase(TestCase):
+class _TestCaseCompat(TestCase):
+    
+    def _callSetUp(self):
+        self.setUp()
+
+    def _callTestMethod(self, method):
+        method()
+
+    def _callTearDown(self):
+        self.tearDown()
+
+    def _callCleanup(self, function, *args, **kwargs):
+        function(*args, **kwargs)
+
+    def run(self, result=None):
+        orig_result = result
+        if result is None:
+            result = self.defaultTestResult()
+            startTestRun = getattr(result, 'startTestRun', None)
+            if startTestRun is not None:
+                startTestRun()
+
+        result.startTest(self)
+
+        testMethod = getattr(self, self._testMethodName)
+        if (getattr(self.__class__, "__unittest_skip__", False) or
+            getattr(testMethod, "__unittest_skip__", False)):
+            # If the class or method was skipped.
+            try:
+                skip_why = (getattr(self.__class__, '__unittest_skip_why__', '')
+                            or getattr(testMethod, '__unittest_skip_why__', ''))
+                self._addSkip(result, self, skip_why)
+            finally:
+                result.stopTest(self)
+            return
+        expecting_failure_method = getattr(testMethod,
+                                           "__unittest_expecting_failure__", False)
+        expecting_failure_class = getattr(self,
+                                          "__unittest_expecting_failure__", False)
+        expecting_failure = expecting_failure_class or expecting_failure_method
+        outcome = _Outcome(result)
+        try:
+            self._outcome = outcome
+
+            with outcome.testPartExecutor(self):
+                self._callSetUp()
+            if outcome.success:
+                outcome.expecting_failure = expecting_failure
+                with outcome.testPartExecutor(self, isTest=True):
+                    self._callTestMethod(testMethod)
+                outcome.expecting_failure = False
+                with outcome.testPartExecutor(self):
+                    self._callTearDown()
+
+            self.doCleanups()
+            for test, reason in outcome.skipped:
+                self._addSkip(result, test, reason)
+            self._feedErrorsToResult(result, outcome.errors)
+            if outcome.success:
+                if expecting_failure:
+                    if outcome.expectedFailure:
+                        self._addExpectedFailure(result, outcome.expectedFailure)
+                    else:
+                        self._addUnexpectedSuccess(result)
+                else:
+                    result.addSuccess(self)
+            return result
+        finally:
+            result.stopTest(self)
+            if orig_result is None:
+                stopTestRun = getattr(result, 'stopTestRun', None)
+                if stopTestRun is not None:
+                    stopTestRun()
+
+            # explicitly break reference cycles:
+            # outcome.errors -> frame -> outcome -> outcome.errors
+            # outcome.expectedFailure -> frame -> outcome -> outcome.expectedFailure
+            outcome.errors.clear()
+            outcome.expectedFailure = None
+
+            # clear the outcome, no more needed
+            self._outcome = None
+
+
+class IsolatedAsyncioTestCase(_TestCaseCompat):
     # Names intentionally have a long prefix
     # to reduce a chance of clashing with user-defined attributes
     # from inherited test case
@@ -42,7 +126,7 @@ class IsolatedAsyncioTestCase(TestCase):
     async def asyncTearDown(self):
         pass
 
-    def addAsyncCleanup(self, func, /, *args, **kwargs):
+    def addAsyncCleanup(self, func, *args, **kwargs):
         # A trivial trampoline to addCleanup()
         # the function exists because it has a different semantics
         # and signature:
@@ -71,7 +155,7 @@ class IsolatedAsyncioTestCase(TestCase):
     def _callCleanup(self, function, *args, **kwargs):
         self._callMaybeAsync(function, *args, **kwargs)
 
-    def _callAsync(self, func, /, *args, **kwargs):
+    def _callAsync(self, func, *args, **kwargs):
         assert self._asyncioTestLoop is not None
         ret = func(*args, **kwargs)
         assert inspect.isawaitable(ret)
@@ -79,7 +163,7 @@ class IsolatedAsyncioTestCase(TestCase):
         self._asyncioCallsQueue.put_nowait((fut, ret))
         return self._asyncioTestLoop.run_until_complete(fut)
 
-    def _callMaybeAsync(self, func, /, *args, **kwargs):
+    def _callMaybeAsync(self, func, *args, **kwargs):
         assert self._asyncioTestLoop is not None
         ret = func(*args, **kwargs)
         if inspect.isawaitable(ret):
